1c38c29ab1da0e978ac263cca7807372bdf85460
pny <pnymeet@gmail.com>
2026-02-15 15:56:41 +0500

    data

--- web/data.js
+++ web/data.js
@@ -1,74 +1,29 @@
-const users = [
-  {
-    id: 1,
-    name: "John Smith",
-    email: "john.smith@example.com",
-    designation: "Software Engineer",
-    department: "IT"
-  },
-  {
-    id: 2,
-    name: "Sarah Johnson",
-    email: "sarah.johnson@example.com",
-    designation: "HR Manager",
-    department: "Human Resources"
-  },
-  {
-    id: 3,
-    name: "Michael Brown",
-    email: "michael.brown@example.com",
-    designation: "Financial Analyst",
-    department: "Finance"
-  },
-  {
-    id: 4,
-    name: "Emily Davis",
-    email: "emily.davis@example.com",
-    designation: "Marketing Executive",
-    department: "Human Resources"
-  },
-  {
-    id: 5,
-    name: "David Wilson",
-    email: "david.wilson@example.com",
-    designation: "Sales Manager",
-    department: "Finance"
-  },
-  {
-    id: 6,
-    name: "Jessica Martinez",
-    email: "jessica.martinez@example.com",
-    designation: "Product Manager",
-    department: "Design"
-  },
-  {
-    id: 7,
-    name: "Daniel Anderson",
-    email: "daniel.anderson@example.com",
-    designation: "UX Designer",
-    department: "Design"
-  },
-  {
-    id: 8,
-    name: "Olivia Thomas",
-    email: "olivia.thomas@example.com",
-    designation: "Data Scientist",
-    department: "Data Analytics"
-  },
-  {
-    id: 9,
-    name: "James Taylor",
-    email: "james.taylor@example.com",
-    designation: "Operations Manager",
-    department: "Operations"
-  },
-  {
-    id: 10,
-    name: "Sophia Moore",
-    email: "sophia.moore@example.com",
-    designation: "Customer Support Lead",
-    department: "Customer Service"
-  }
+const array1 = [
+  "Ali",
+  "Lahore",
+  34,
+  67,
+  "Alex",
+  "C",
+  "A"
 ];
 
-export default users; // optional (if using ES modules)
+const numbers = [];
+const words = [];
+const letters = [];
+array1.map( (item) => {
+  if (typeof item == "number") {
+    numbers.push(item);
+  }else if(typeof item == "string" && item.length == 1){
+    words.push(item)
+  } else {
+    letters.push(item)
+  }
+});
+
+
+
+
+
+console.log(numbers, words, letters)
+// hint: typeof;
