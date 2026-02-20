7049433ef7916036507fd93af000dab8b5d1ddda
pny <pnymeet@gmail.com>
2026-02-20 13:21:55 +0500

    button assignment

--- web/reactjs/first-project/src/App.jsx
+++ web/reactjs/first-project/src/App.jsx
@@ -1,22 +1,32 @@
+import { useState } from "react"
 import Button from "./components/Button"
 import Heading from "./components/Heading"
 
 const App = () => {
+  const [name, setName] = useState("Ali");
+
+  const [ counter, setCounter ] = useState(0)
 
   const btn1 = () => {
-    console.log("Button 1 clicked")
+    setName("Muneeb")
+    setCounter(counter + 1);
   }
   const btn2 = () => {
-    console.log("Button 2 clicked")
+    if (counter < 1) return;
+    
+    setName("Ali")
+    setCounter(counter - 1)
   }
 
   return (
     <div>
       <Heading />
+      {name}
+      <hr />
 
-      <Button func={btn1} title="Button 1"/>
-      <br />
-      <Button func={btn2} title="Button 2"/>
+      <Button isToggle={true} func={btn1} title="Increment"/> {" "}
+      <big>{counter}</big> {" "}
+      <Button isToggle={true} func={btn2} title="Decrement"/>
 
     </div>
   )
--- web/reactjs/first-project/src/components/Button.jsx
+++ web/reactjs/first-project/src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ title, func }) => {
+const Button = ({ title, func, isToggle }) => {
   return (
-    <button onClick={func}>{title}</button>
+    isToggle && <button onClick={func}>{title}</button>
   )
 }
 
