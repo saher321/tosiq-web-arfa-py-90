b61007b3da537ce402f8daf6637abd250e69ee06
pny <pnymeet@gmail.com>
2026-03-07 13:29:57 +0500

    python intro

---
+++ web/python_lectures/main.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter number 1: "))
+# num2 = int(input("Enter number 2: "))
+if num1 > 0:
+    print("This is positive number")
+else:
+    print("This is negative number")
+
+
+# assignment 7/3/2026 => get value from user and find its leap year
---
+++ web/python_lectures/python1.py
@@ -0,0 +1,10 @@
+# For user input when running code: The "Output" panel is read-only. 
+# You must switch to the "Terminal" panel for interactive input.
+# To fix this permanently for extensions like Code Runner, enable the setting to run code in the integrated terminal:
+# Open Settings (File > Preferences > Settings or Code > Settings on macOS, or press Ctrl+,).
+# Search for code-runner.runInTerminal.
+# Check the box for "Code Runner: Run In Terminal".
+# Restart VS Code
+print("Program is starting...")
+print(input("Enter number: "))
+print("End of program!")
