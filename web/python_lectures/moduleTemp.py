c3d632d7f117a4a04791c1b079ed08a9778f1b28
pny <pnymeet@gmail.com>
2026-03-08 12:41:43 +0500

    temp module py

---
+++ web/python_lectures/moduleTemp.py
@@ -0,0 +1,7 @@
+def cToF(c):
+    f = 9/5 * c + 32
+    print("C to F value result:", f)
+
+def fToC(f):
+    c = 5/9 * f - 32
+    print("F to C value result:", c)
---
+++ web/python_lectures/temp.py
@@ -0,0 +1,7 @@
+import moduleTemp
+
+c = float(input("Enter temperature in Cel: "))
+moduleTemp.cToF(c)
+
+f = float(input("Enter temperature in Fah: "))
+moduleTemp.fToC(f)
