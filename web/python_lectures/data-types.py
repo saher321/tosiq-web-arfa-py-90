5ca62ca15b7c2c09b99421b8683d61bf760faa11
pny <pnymeet@gmail.com>
2026-03-08 13:10:37 +0500

    data types in python

---
+++ web/python_lectures/data-types.py
@@ -0,0 +1,29 @@
+# PYTHON DATA TYPES
+
+# int       => 123
+# float     => 12.43
+# str       => "Hello from python"
+# bool      => True/False
+# list      => ["Black", "Purple", "Yellow"]
+# dictionary=> {
+    # name: "Ali"
+    # age: 23
+# }
+
+# list of dictionary => [ {name: "Ali"}, {name: "John"} ]
+
+# tuple     => (1,2,3,4,5,6,7,7) :: immutable -> non changeable
+# set       => {1,3,4,6,8,9} :: mutable -> changeable
+
+a = 1.23
+b = 12.3
+c = "Hello"
+d = True
+
+print ("-------Data types-------")
+print ("The isInstance func. of a is:", isinstance(a, int))
+print ("The type of a is", type(a))
+print ("The type of b is", type(b))
+print ("The type of c is", type(c))
+print ("The type of d is", type(d))
+print ("-------Data types-------")
