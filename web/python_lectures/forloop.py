a3b9ed03292e4d77a60b96071ab50a8a849b9e66
= <pnymeet@gmail.com>
2026-03-14 13:51:52 +0500

    loop

--- web/python_lectures/forloop.py
+++ web/python_lectures/forloop.py
@@ -15,8 +15,22 @@ num = int(input("Enter table number: "))
 print("*******Printing table of", num, "*******")
 
 # 2 * 1 = 2
-for i in range(1, 21):
+for i in range(1, 11):
     print(num, " * ", i, " = ", num*i)
 
+# ****** Assignment ******
+# Create a tables according to user defined
 
+# input :
+# Enter table number: 3
+# Enter range of table: 50
+
+# output: 
+# 3 * 1 = 2
+#
+# user defined range
+#
+# 3 * 50 = ?
+
+# hint: nested loop
 
