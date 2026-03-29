33ae5c82700b5665547295d258783e03b2f1f4e1
tosiq <pnymeet@gmail.com>
2026-03-29 15:19:08 +0500

    error fixed, apis tested and created

--- web/python_lectures/django-lectures/blogger/backend/blog/urls.py
+++ web/python_lectures/django-lectures/blogger/backend/blog/urls.py
@@ -4,8 +4,8 @@ from .views import PostViewSet
 router = DefaultRouter()
 router.register('posts', PostViewSet)
 urlpatterns = router.urls
-# GET /posts/ => list of posts
-# GET /posts/1/ => details of post with id 1
-# POST /posts/ => create a new post
-# PUT /posts/1/ => update post with id 1
-# DELETE /posts/1/ => delete post with id 1
+# GET       http://127.0.0.1:8000/api/posts/    :: list of posts
+# GET       http://127.0.0.1:8000/api/posts/1/  :: details of post with id 1
+# POST      http://127.0.0.1:8000/api/posts/    :: create a new post
+# PUT       http://127.0.0.1:8000/api/posts/1/  :: update post with id 1
+# DELETE    http://127.0.0.1:8000/api/posts/1/  :: delete post with id 1
