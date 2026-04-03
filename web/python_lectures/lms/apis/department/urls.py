c4373df80ec2178145ca2810aa9baaf29c8fe67a
Muhammad Talha <pnymeet@gmail.com>
2026-04-03 15:35:00 +0500

    department created

--- web/python_lectures/lms/apis/department/urls.py
+++ web/python_lectures/lms/apis/department/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import departments
+from .views import departments, create
 
 urlpatterns = [
-    path('departments/', departments)
+    path('departments/', departments),
+    path('departments/create', create)
 ]
--- web/python_lectures/lms/apis/department/views.py
+++ web/python_lectures/lms/apis/department/views.py
@@ -14,3 +14,27 @@ def departments(request):
         'data' : dsr.data
     })
 
+@api_view(['POST'])
+def create(request):
+
+    if not request.data:
+        return Response({
+            'status': False,
+            'message': "Please fill all remaining fields"
+        })
+    
+    deptData = DepartmentSerializer(data=request.data)
+    if deptData.is_valid():
+        deptData.save()
+        return Response({
+            'status': True,
+            'message': "Data has been created"
+        })
+    else:
+        return Response({
+            'status': False,
+            'message': "Failed to create data"
+        })
+
+
+
