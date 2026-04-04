0aebd7070f862124dd1c572d82da3219f12e8d3a
pny <pnymeet@gmail.com>
2026-04-04 14:41:21 +0500

    update delete url

--- web/python_lectures/lms/apis/department/urls.py
+++ web/python_lectures/lms/apis/department/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('departments/', departments),
     path('departments/create', create),
-    path('departments/delete/{int:pk}', delete)
+    path('departments/delete/<int:pk>', delete)
 ]
 
 #  a = 10 => <class int>
