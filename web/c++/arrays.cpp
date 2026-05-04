5aeef98d763f8983989394554f7777a59ef084a7
pny <pnymeet@gmail.com>
2026-05-04 10:12:26 +0500

    arrays

--- web/c++/arrays.cpp
+++ web/c++/arrays.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main () {
 	string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
-	
+	int even[];
+	int odd[];
+	int numbers[10] = {-44, -221, 1, 4, 245, 78, 3, 8, 53, 99}
 	for (int i=0; i < (sizeof(cars)-1); i++ ){
 		cout << "Car Brands: " << cars[i] << endl;
 	}
