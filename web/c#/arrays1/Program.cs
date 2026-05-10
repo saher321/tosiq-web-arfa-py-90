2cfae003e9400092b55aa88229cf39f51f3c12a5
Tosiq <pnymeet@gmail.com>
2026-05-10 15:49:57 +0500

    arrays, 10 may 2026 assignment

---
+++ web/c#/arrays1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 44, 23, 34, 30, 45 };
+
+        int subjects = Convert.ToInt32(Console.ReadLine("ENTER SUBJECTS: "));
+
+        int[] sub = new int[subjects];
+
+        double obtained = 0;
+        double total = 250;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            obtained = obtained + numbers[i];
+        }
+
+        double prcnt = (obtained / total) * 100;
+        Console.WriteLine("Obtained: " + obtained);
+        Console.WriteLine("Total: " + total);
+        Console.WriteLine("Obtained Percentage: " + prcnt.ToString("F2") + "%");
+    }
+}
+
+/// Assignment 
+/// ENTER SUBJECTS: 4,5,
+/// SUBJECT 1 MARKS: 44
+/// SUBJECT 2 MARKS: 23
+/// SUBJECT 3 MARKS: 34
+/// SUBJECT 4 MARKS: 30
+/// SUBJECT 5 MARKS: 45
+/// OBTAINED: 176
+/// TOTAL: 250
+/// OBTAINED PERCENTAGE: 70.40%
---
+++ web/c#/sumofproduct/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int product = 1;
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i % 2 == 1)
+            {
+                product = product * i;
+            }
+        }
+        Console.WriteLine("Product of first 10 odd numbers: " + product);
+    }
+}
